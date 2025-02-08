6df00549d4282a8a0f43d1f1fc0ac4deffff8a22
puco2023 <136252052+puco2023@users.noreply.github.com>
2025-02-08 20:49:44 +0100

    Update combine.py

--- combine.py
+++ combine.py
@@ -1,4 +1,3 @@
-#solution 1
 class Solution(object):
     def combine(self, n, k):
         ans=[]
@@ -17,21 +16,4 @@ class Solution(object):
             curr.pop()
             dfs(j+1)
         dfs(0)
-        return ans
-#solution 2
-class Solution(object):
-    def combine(self, n, k):
-        ans=[]
-        curr=[]
-        def dfs(j):
-            if len(curr) == k:
-                ans.append(curr.copy())
-                return
-            if j>=n:
-                return
-            for i in range(j,n):
-                curr.append(i)
-                dfs(j+1)
-                curr.pop()
-        dfs(1)
         return ans
